6cf47dbe44e259190f0aafd3b1662b9d093c76b8
Christoph Groth <christoph.groth@cea.fr>
2012-07-22 12:54:08 +0200

    slicer: some optimizations in python wrapper
    Instead of a tuple of tuples of python integers, return a list of numpy arrays.
    Initialize the arrays by memcpy.

--- kwant/graph/slicer.pyx
+++ kwant/graph/slicer.pyx
@@ -1,6 +1,7 @@
 import numpy as np
 cimport numpy as np
 cimport cython
+from libc.string cimport memcpy
 from kwant.graph.defs cimport gint
 from kwant.graph.defs import gint_dtype
 from kwant.graph.core cimport CGraph
@@ -15,7 +16,7 @@ def slice(CGraph graph, left, right):
     """
     cdef np.ndarray[gint, ndim=1] leftarr, rightarr, slc
     cdef c_slicer.Slicing *slicing
-    cdef int i, j, slc_size
+    cdef int i, slc_size
 
     leftarr = np.unique(np.array(left, dtype=gint_dtype))
     rightarr = np.unique(np.array(right, dtype=gint_dtype))
@@ -39,19 +40,12 @@ def slice(CGraph graph, left, right):
                              graph.heads,
                              leftarr.size, <gint *>leftarr.data,
                              rightarr.size, <gint *>rightarr.data)
-
-    slclist = []
+    slices = []
     for i in xrange(slicing.nslices):
         slc_size = slicing.slice_ptr[i+1] - slicing.slice_ptr[i]
         slc = np.empty(slc_size, dtype=gint_dtype)
-
-        # I don't know, there should be a better way to do that.
-        cslc = slicing.slices + slicing.slice_ptr[i]
-        for j in xrange(slc_size):
-            slc[j] = cslc[j]
-
-        slclist.append(tuple(slc))
-
+        memcpy(slc.data, slicing.slices + slicing.slice_ptr[i],
+               sizeof(gint) * slc_size)
+        slices.append(slc)
     c_slicer.freeSlicing(slicing)
-
-    return slclist
+    return slices
