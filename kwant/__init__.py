255ccd92295e2152e1eb97574016b3b3777cb31b
Christoph Groth <christoph.groth@cea.fr>
2012-11-08 16:44:34 +0100

    allow tests to be run without an inplace build

--- kwant/__init__.py
+++ kwant/__init__.py
@@ -20,10 +20,29 @@ except:
 else:
     __all__.extend(['plotter', 'plot'])
 
-# Mumps usally works best, use scipy_sparse as fallback
+# Mumps usally works best, use sparse as fallback
 try:
     from .solvers.mumps import solve
     __all__.append('solve')
 except ImportError:
     from .solvers.sparse import solve
     __all__.append('solve')
+
+def runtest():
+    """Run the tests included with kwant."""
+    import os.path
+    try:
+        import nose
+    except ImportError:
+        msg = 'No module named nose.\n(See INSTALL.txt in ' \
+            'the top-level directory of the kwant distribution.)'
+        raise ImportError(msg)
+    kwant_dir = os.path.dirname(__file__)
+    if os.path.basename(kwant_dir) == kwant_dir:
+        msg = 'Do not call kwant.test from within the kwant source ' \
+        'distribution directory.\nYou can run "nosetests -s" instead.'
+        raise RuntimeError(msg)
+    nose.run(defaultTest=kwant_dir)
+
+# This trick avoids recursive execution of tests by nosetests.
+test = runtest
