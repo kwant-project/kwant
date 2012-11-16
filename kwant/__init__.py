ef361d46cd3e8f366a96199a07cb0fef4b485e8f
Christoph Groth <christoph.groth@cea.fr>
2012-11-16 15:35:55 +0100

    stop advertising the running of tests to end users
    I've realized that this was broken. (The tests were not installed and could be
    only run for inplace-builds linked from somewhere else.)  Fixing this would be
    somewhat hacky.  I think now that it was a bad idea in the first place to
    advertise the running of unit tests to end users.

--- kwant/__init__.py
+++ kwant/__init__.py
@@ -27,22 +27,3 @@ try:
 except ImportError:
     from .solvers.sparse import solve
     __all__.append('solve')
-
-def runtest():
-    """Run the tests included with kwant."""
-    import os.path
-    try:
-        import nose
-    except ImportError:
-        msg = 'No module named nose.\n(See INSTALL.txt in ' \
-            'the top-level directory of the kwant distribution.)'
-        raise ImportError(msg)
-    kwant_dir = os.path.dirname(__file__)
-    if os.path.basename(kwant_dir) == kwant_dir:
-        msg = 'Do not call kwant.test from within the kwant source ' \
-        'distribution directory.\nYou can run "nosetests -s" instead.'
-        raise RuntimeError(msg)
-    nose.run(defaultTest=kwant_dir)
-
-# This trick avoids recursive execution of tests by nosetests.
-test = runtest
