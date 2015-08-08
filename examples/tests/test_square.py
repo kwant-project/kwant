714daa2659ebff7801366796fab1bdd790de4ad2
Anton Akhmerov <anton.akhmerov@gmail.com>
2015-08-08 21:55:32 +0200

    update examples to py3

--- examples/tests/test_square.py
+++ examples/tests/test_square.py
@@ -4,18 +4,18 @@ from numpy.testing import assert_almost_equal
 
 def test_nodeid_to_from_pos():
     s = square.System((3, 4), 1)
-    assert_raises(StandardError, s.nodeid_from_pos, (0, -2))
-    assert_raises(StandardError, s.nodeid_from_pos, (-1, 3))
-    assert_raises(StandardError, s.nodeid_from_pos, (3, 1))
-    assert_raises(StandardError, s.pos_from_nodeid, -1)
-    assert_raises(StandardError, s.pos_from_nodeid, 12)
+    assert_raises(Exception, s.nodeid_from_pos, (0, -2))
+    assert_raises(Exception, s.nodeid_from_pos, (-1, 3))
+    assert_raises(Exception, s.nodeid_from_pos, (3, 1))
+    assert_raises(Exception, s.pos_from_nodeid, -1)
+    assert_raises(Exception, s.pos_from_nodeid, 12)
     assert_equal(s.nodeid_from_pos((0, 0)), 0)
     assert_equal(s.nodeid_from_pos(s.pos_from_nodeid(7)), 7)
     assert_equal(s.pos_from_nodeid(s.nodeid_from_pos((2, 3))), (2, 3))
 
 def test_hamiltonian():
     sys = square.System((4, 5), 1)
-    for i in xrange(sys.graph.num_nodes):
+    for i in range(sys.graph.num_nodes):
         shape = sys.hamiltonian(i, i).shape
         assert_equal(len(shape), 2)
         assert_equal(shape[0], 1)
@@ -28,7 +28,7 @@ def test_hamiltonian():
 
 def test_selfenergy():
     sys = square.System((2, 4), 1)
-    for lead in xrange(len(sys.lead_interfaces)):
+    for lead in range(len(sys.lead_interfaces)):
         se = sys.leads[lead].selfenergy(0)
         assert_equal(len(se.shape), 2)
         assert_equal(se.shape[0], se.shape[1])
