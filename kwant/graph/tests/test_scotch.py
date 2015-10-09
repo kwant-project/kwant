c2b5f33989757e02e66975309a7cae934eb7e2f5
Christoph Groth <christoph.groth@cea.fr>
2015-10-09 16:21:24 +0200

    update copyright notices

--- kwant/graph/tests/test_scotch.py
+++ kwant/graph/tests/test_scotch.py
@@ -1,9 +1,9 @@
 # Copyright 2011-2013 Kwant authors.
 #
-# This file is part of Kwant.  It is subject to the license terms in the
-# LICENSE file found in the top-level directory of this distribution and at
+# This file is part of Kwant.  It is subject to the license terms in the file
+# LICENSE.rst found in the top-level directory of this distribution and at
 # http://kwant-project.org/license.  A list of Kwant authors can be found in
-# the AUTHORS file at the top-level directory of this distribution and at
+# the file AUTHORS.rst at the top-level directory of this distribution and at
 # http://kwant-project.org/authors.
 
 from nose.tools import assert_true
