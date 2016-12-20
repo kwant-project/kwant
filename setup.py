d26cc8ca0936b3029ebd91ff5aaa2d0b18185e0e
Christoph Groth <christoph.groth@cea.fr>
2016-12-20 17:44:19 +0100

    setup.py: get rid of tests_require
    It has the same problems as setup_requires.

--- setup.py
+++ setup.py
@@ -616,7 +616,6 @@ def main():
                     'build_tut': build_tut,
                     'test': test},
           ext_modules=exts,
-          tests_require=['numpy > 1.6.1', 'pytest >= 2.6.3'],
           install_requires=['numpy > 1.6.1', 'scipy >= 0.11.0', 'tinyarray'],
           extras_require={'plotting': 'matplotlib >= 1.2'},
           classifiers=[c.strip() for c in classifiers.split('\n')])
