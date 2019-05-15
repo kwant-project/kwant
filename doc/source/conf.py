53dfb1e8ebedf4f6bb281d768ad60ed33f7bf69d
Joseph Weston <joseph@weston.cloud>
2019-05-15 15:17:08 +0200

    convert SVG to PDF when using latex builder

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -38,7 +38,8 @@ sys.path.insert(0, os.path.abspath('../sphinxext'))
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary',
               'sphinx.ext.todo', 'sphinx.ext.mathjax', 'numpydoc',
-              'kwantdoc', 'sphinx.ext.linkcode', 'jupyter_sphinx.execute']
+              'kwantdoc', 'sphinx.ext.linkcode', 'jupyter_sphinx.execute',
+              'sphinxcontrib.rsvgconverter']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['../templates']
