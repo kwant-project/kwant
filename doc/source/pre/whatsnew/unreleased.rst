ae797dc841a80cc7e0283f37952f26d5e66daf59
Anton Akhmerov <anton.akhmerov@gmail.com>
2024-01-07 13:27:49 +0100

    remove code for specially treating umfpack solver
    We relied on accessing private namespace in scipy, which is bound to break and
    emits deprecation warnings. Additionally I verified that there is no way for a
    user to obtain a functioning scikit-umfpack aside of building it from source.
    This makes the additional effort of implementing an own solver negligible.

--- doc/source/pre/whatsnew/unreleased.rst
+++ doc/source/pre/whatsnew/unreleased.rst
@@ -11,5 +11,11 @@ removed. Instead the users should run ``py.test`` directly or use
 ``import kwant; kwant.test()``.
 
 The minimum required version of Python is now 3.8 (the previous versions are
-past end of life). The other packages required by Kwant are also updated to
+past end of life).  The other packages required by Kwant are also updated to
 their versions available for Python 3.8.
+
+Removal of Umfpack support
+--------------------------
+Scipy used to provide a Python interface to the Umfpack library.  This is now
+done by a separate package ``scikit-umfpack``.  Because it is hard for end
+users to obtain this, we have removed built-in support for Umfpack.
--- kwant/solvers/sparse.py
+++ kwant/solvers/sparse.py
@@ -10,70 +10,9 @@ __all__ = ['smatrix', 'greens_function', 'ldos', 'wave_function', 'Solver']
 
 import numpy as np
 import scipy.sparse as sp
+from scipy.sparse.linalg import factorized
+
 from . import common
-
-# Note: previous code would have failed if UMFPACK was provided by scikit
-from scipy.sparse.linalg.dsolve import linsolve
-
-uses_umfpack = linsolve.useUmfpack
-
-if uses_umfpack:
-    def factorized(A, piv_tol=1.0, sym_piv_tol=1.0):
-        """
-        Return a fuction for solving a sparse linear system, with A
-        pre-factorized.
-
-        Parameters
-        ----------
-        A : csc_matrix
-            matrix to be factorized
-        piv_tol : float, 0 <= piv_tol <= 1.0
-        sym_piv_tol : float, 0 <= piv_tol <= 1.0
-            thresholds used by UMFPACK for pivoting. 0 means no pivoting, 1.0
-            means full pivoting as in dense matrices (guaranteeing stability,
-            but reducing possibly sparsity). Defaults of UMFPACK are 0.1 and
-            0.001 respectively. Whether piv_tol or sym_piv_tol are used is
-            decided internally by UMFPACK, depending on whether the matrix is
-            "symmetric" enough.
-
-        Examples
-        --------
-        solve = factorized(A) # Makes LU decomposition.
-        x1 = solve(rhs1) # Uses the LU factors.
-        x2 = solve(rhs2) # Uses again the LU factors.
-        """
-        umfpack = linsolve.umfpack
-
-        if not sp.isspmatrix_csc(A):
-            A = sp.csc_matrix(A)
-
-        A.sort_indices()
-        A = A.asfptype()  # upcast to a floating point format
-
-        if A.dtype.char not in 'dD':
-            raise ValueError("convert matrix data to double, please, using"
-                             " .astype()")
-
-        family = {'d': 'di', 'D': 'zi'}
-        umf = umfpack.UmfpackContext(family[A.dtype.char])
-
-        # adjust pivot thresholds
-        umf.control[umfpack.UMFPACK_PIVOT_TOLERANCE] = piv_tol
-        umf.control[umfpack.UMFPACK_SYM_PIVOT_TOLERANCE] = sym_piv_tol
-
-        # Make LU decomposition.
-        umf.numeric(A)
-
-        def solve(b):
-            return umf.solve(umfpack.UMFPACK_A, A, b, autoTranspose=True)
-
-        return solve
-else:
-    # no UMFPACK found. SuperLU is being used, but usually abysmally slow
-    # (SuperLu is not bad per se, somehow the SciPy version isn't good).
-    # Since scipy doesn't include UMFPACK anymore due to software rot,
-    # there is no warning here.
-    factorized = linsolve.factorized
 
 
 class Solver(common.SparseSolver):
